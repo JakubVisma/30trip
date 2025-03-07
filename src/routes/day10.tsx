29eb8708471394b7c021cb02e0a7a4db5ad889b6
Jakub Brzosko <jakub.brzosko@visma.com>
2025-03-07 10:26:41 +0100

    add coordinates

--- src/routes/day10.tsx
+++ src/routes/day10.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import qr1 from './day10/qr1.png';
 
 function Day10() {
     const date = new Date().getDate();
-    if(date<10){
-        return(
+    if (date < 10) {
+        return (
             <h1 style={{ color: 'red' }}>Proszę nie kombinować!</h1>
         );
     }
@@ -11,16 +12,11 @@ function Day10() {
     return (
         <div>
             <h1 style={{ color: "white", marginLeft: "5rem" }}>
-                <p>Dzień 10</p>
+                <p>Dzień 10 - W nieznane!</p>
             </h1>
-            <div className="m-5 pb-15">
-                <p >Cześć Misiaku! Witem Cię w tym pięknym miesiącu, kiedy skończysz 30 latek. O rany ale to zleciało!</p>
-                <p >Z tej okazji chiałbym zabrać Cię w podróż. Każdego dnia jedna kartka z kalendarza będzie się aktywować. Każda z nich to jakieś unikalne wspomnienie, wyzwanie, prezent albo gra. Jeżeli uda Ci się ukończyć wszystkie z nich, czeka Cię -tytyrytyty- Nagroda Główna!</p>
-                <p > Mam nadzieję że te podróż sprawi Ci radość, tak jak inne nasze podróże. Wszystkiego najlepszego po raz pierwszy. Kocham Cię! </p>      
-                <div className="">
-            <h4 className="mt-5" style={{color: 'white'}}> Każda podróż zaczyna się od pierwszego kroku. My wykonaliśmy razem wiele pierwszych z nich. Czy pamiętasz je wszystkie?</h4>
-
-            </div>          
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+                <img style={{ width: '50%' }} src={qr1} className="mt-5" />
+                <p> </p>
             </div>
 
 
