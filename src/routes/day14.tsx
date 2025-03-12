e3a45f3c31f69b008dd5eddebf8f2ea26e062f72
Jakub Brzosko <jakub.brzosko@visma.com>
2025-03-12 13:18:12 +0100

    day14

--- src/routes/day14.tsx
+++ src/routes/day14.tsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import finale from "./day14/finale.mp4"
+import { Button, TextField } from "@mui/material";
 
 function Day14() {
+    const [passwordAttempts, setPasswordAttempts] = useState(0);
+    const [password, setPassword] = useState('');
     const date = new Date().getDate();
-    if(date<14){
-        return(
+    if (date < 14) {
+        return (
             <h1 style={{ color: 'red' }}>Proszę nie kombinować!</h1>
         );
     }
+function checkPassword(){
+    setPasswordAttempts(passwordAttempts+1);
+    setPassword('');
+}
 
     return (
         <div>
             <h1 style={{ color: "white", marginLeft: "5rem" }}>
-                <p>Dzień 14</p>
+                <p>Dzień 14 - Grande Finale</p>
             </h1>
             <div className="m-5 pb-15">
-                <p >Cześć Misiaku! Witem Cię w tym pięknym miesiącu, kiedy skończysz 30 latek. O rany ale to zleciało!</p>
-                <p >Z tej okazji chiałbym zabrać Cię w podróż. Każdego dnia jedna kartka z kalendarza będzie się aktywować. Każda z nich to jakieś unikalne wspomnienie, wyzwanie, prezent albo gra. Jeżeli uda Ci się ukończyć wszystkie z nich, czeka Cię -tytyrytyty- Nagroda Główna!</p>
-                <p > Mam nadzieję że te podróż sprawi Ci radość, tak jak inne nasze podróże. Wszystkiego najlepszego po raz pierwszy. Kocham Cię! </p>      
-                <div className="">
-            <h4 className="mt-5" style={{color: 'white'}}> Każda podróż zaczyna się od pierwszego kroku. My wykonaliśmy razem wiele pierwszych z nich. Czy pamiętasz je wszystkie?</h4>
 
-            </div>          
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+                    {passwordAttempts==0 ?  <p >Mam nadzieję że pilnowałaś literek. Wpisz hasło i sprawdźmy co się stanie</p> : <p  style={{ color: `rgb(250, 0, 0)`}} >Problem. Spróbuj jeszcze raz!</p>}
+                    <div style={{ backgroundColor: `rgb(46,46,46)`, width:'150px', borderRadius:'5px' }}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Hasło"
+                        variant="standard"
+                        color="info"
+                        value={password}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setPassword(event.target.value) }} />
+                    
+                        </div>
+                        <Button onClick={checkPassword}> Sprawdź hasło.</Button>
+                    {passwordAttempts > 2 &&
+                        <>
+                            < video src={finale} width="600" height="300" controls="controls" />
+                            <p style={{ color: `rgb(212, 142, 12)`}}>Wygląda na to że mamy problem. Nawiązuję połączenie z konsultantem.</p>
+                            <h5 className="mt-5" style={{ color: 'white' }}> To by było na tyle. Mam nadzieję że dobrze się bawiłaś przez ostatnie 14 dni, a dzień dzisiejszy będzie wisienką na torcie. Buziaki :* </h5>
+                            <h5 style={{ color: 'grey' }}>Twój Mąż ❤️</h5>
+                        </>}
+
+                </div>
+
             </div>
 
-
-        </div>
+        </div >
     );
 }
 
