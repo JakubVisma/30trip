fb872879c86800cad49371e2b3e058158756c35e
Jakub Brzosko <jakub.brzosko@visma.com>
2025-03-13 11:50:08 +0100

    add loading

--- src/routes/day14.tsx
+++ src/routes/day14.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import finale from "./day14/finale.mp4"
-import { Button, TextField } from "@mui/material";
+import { Button, CircularProgress, TextField } from "@mui/material";
 
 function Day14() {
     const [passwordAttempts, setPasswordAttempts] = useState(0);
     const [password, setPassword] = useState('');
+    const [showVideo, setShowVideo] = useState(false);
+    const [showLoading, setShowLoading] = useState(true);
     const date = new Date().getDate();
     if (date < 14) {
         return (
             <h1 style={{ color: 'red' }}>Proszę nie kombinować!</h1>
         );
     }
+
+
 function checkPassword(){
     setPasswordAttempts(passwordAttempts+1);
     setPassword('');
+    if(passwordAttempts>1)
+    {
+        setTimeout(function() {
+            setShowLoading(false);
+            setShowVideo(true);
+       }, 8000);
+    }
 }
+
 
     return (
         <div>
@@ -26,6 +38,7 @@ function checkPassword(){
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
                     {passwordAttempts==0 ?  <p >Mam nadzieję że pilnowałaś literek. Wpisz hasło i sprawdźmy co się stanie</p> : <p  style={{ color: `rgb(250, 0, 0)`}} >Problem. Spróbuj jeszcze raz!</p>}
                     <div style={{ backgroundColor: `rgb(46,46,46)`, width:'150px', borderRadius:'5px' }}>
+                        
                     <TextField
                         id="outlined-basic"
                         label="Hasło"
@@ -38,7 +51,8 @@ function checkPassword(){
                         <Button onClick={checkPassword}> Sprawdź hasło.</Button>
                     {passwordAttempts > 2 &&
                         <>
-                            < video src={finale} width="600" height="300" controls="controls" />
+                            {showLoading && <CircularProgress />}
+                            {showVideo && < video src={finale} width="600" height="300" controls="controls" />}
                             <p style={{ color: `rgb(212, 142, 12)`}}>Wygląda na to że mamy problem. Nawiązuję połączenie z konsultantem.</p>
                             <h5 className="mt-5" style={{ color: 'white' }}> Twój prezent główny to bilet na koncert mumfordów. Gratulacje! </h5>
                             <h5 className="mt-5" style={{ color: 'white' }}> To by było na tyle. Mam nadzieję że dobrze się bawiłaś przez ostatnie 14 dni, a dzień dzisiejszy będzie wisienką na torcie. Buziaki :* </h5>
